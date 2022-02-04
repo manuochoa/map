fd81b53108dd6ebbfcb035aac943b11ccc83aae2
Pinkmann <korgov.vova@icloud.com>
2022-02-04 11:10:22 +0200

    fix

--- src/components/Treasure.js
+++ src/components/Treasure.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import bnbButton from "../images/chests/bnb-button.png";
+import welandButton from "../images/chests/weland-button.png";
 
 export default function Treasure() {
     const [chests, setChests] = useState(["closed", "closed", "closed", "closed", "closed"]);
@@ -18,7 +20,14 @@ export default function Treasure() {
                     {chests.map((item, index) => {
                         return (
                             <li className={"map__chest map__chest--" + chests[index]} key={index} onClick={() => toggleChest(index)}>
-                                <button className="map__chest-button"></button>
+                                <button className="map__chest-button map__chest-button--closed"></button>
+                                <button className="map__chest-button map__chest-button--opened"></button>
+                                <button className="map__chest-action map__chest-action--bnb">
+                                    <img src={bnbButton} className="map__chest-action-icon" alt="bnb" />
+                                </button>
+                                <button className="map__chest-action map__chest-action--weland">
+                                    <img src={welandButton} className="map__chest-action-icon" alt="bnb" />
+                                </button>
                             </li>
                         );
                     })}
